455f484a6af43843a4f3d5f29b3a5b079002abdc
iorlenko <iorlenko@mail.ru>
2016-10-03 23:01:32 +0300

    routes

--- Demo1/src/components/App.js
+++ Demo1/src/components/App.js
@@ -1,1 +1,18 @@
-import React,
+import React, {PropTypes} from 'React';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+          <p>Header here...</p>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
